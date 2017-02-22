d9e010a23ed795b0dbfc26a397803f1564c9876c
t3ndai <Dzonga@Macintosh-2.local>
2017-02-21 21:37:32 -0500

    Initial Commit

---
+++ DollarTranscriptClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DollarTranscriptClient
+//
+//  Created by Tendai Prince Dzonga on 2/21/17.
+//  Copyright © 2017 Tendai Prince Dzonga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
